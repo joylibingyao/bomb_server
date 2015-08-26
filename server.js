5792500ad49cb1e4df108d3eaadea9a27d165a54
joylibingyao <joylibingyao@gmail.com>
2015-08-25 22:17:59 -0700

    Can dropBomb score works regonize user

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5008));
 
 app.get('/', function(req, res) {
-    res.render('index');
+	    res.render('index');
 });
 require('./config/mongoose.js');
 // Connecting with mongoose
@@ -18,69 +18,118 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 
-var bomb = 0 
-
-var arr = []
-var bombloc = {lat: 37.377063618076384, lon: -121.91212448302834
-}
-
-
 var server = app.listen(app.get('port'), function() {
-    console.log('listening on port', app.get('port'));
+	    console.log('listening on port', app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function (socket) {
-    console.log('SERVER::WE ARE USING SOCKETS!!!!!');
-    console.log(socket.id);
+io.sockets.on('connection', function (socket) 
+{
+	var user 
+	var all
+	User.find({}, function(err, results) {
+       if(err) {
+         console.log(err);
+       } else {
+         // res.json(results);
+         all = results
 
+       }
+   })
 
-	socket.on("bomb", function(data) {
-        console.log("bomb2");
-        x = data.x
-        y = data.y
-        console.log(x,y);
-        io.sockets.emit("bomb", {
-            x: x, y: y
-        });
-	
-		var distance
-		var ho=Math.pow((x-bombloc.lat),2)
-		var ver= Math.pow((y-bombloc.lon),2)
-		distance = Math.sqrt(ho+ver)
-		console.log(distance)
-		if (distance < 0.00007) {
-			bomb = true
-			console.log("U r bombed")
-		};
-    });
+	console.log('SERVER::WE ARE USING SOCKETS!!!!!');
+	 console.log(socket.id);
+	socket.on("startPressed", function(data) 
+	{
+		// add to dataBase
+		
+		console.log(data.name);
+		User.find({name:data.name},function(err, results) {
 
-	socket.on("startGame", function(data) {
-		console.log("startGame");
-		console.log(data)
+			if(err) {
+				console.log(err);
+			} else {
+				if (results.length == 0)
+				{
+					console.log("new");
+					user = new User({name:data.name})
+					io.emit("userInfo", 0)
+					user.save(function(err) {
+
+						if(err) 
+						{
+							console.log('something went wrong');
+					    } else { // else console.log that we did well and then redirect to the root route
+					    	console.log('successfully added!');
+					    	
+					    }
+					});
+				}
+				else{
+					user = results
+
+					io.emit("userInfo", user[0].score)
+				}
+			}
+		});
+
+		// added
 		io.emit("geolocation", {
-			name: name,
-			lat: lat,
-			lon: lon
+			name: data.name,
+			lat: data.lat,
+			lon: data.lon
 		})
 	})
 
-	socket.on("name", function(data) {
-		console.log(data)
-		io.emit("name", {
-			name: name
-		})
-	})	
+	//next event
+	socket.on("dropPressed", function(data) 
+	{
+	console.log("bomb2",data);
+	// add bomb to dataBase
 
-	socket.on("status", function(data) {
+	User.update( {name : data.name},{
+			$inc:{score:1},
+			$addToSet : { bombs : {lat:data.lat, lon:data.lon} }
+		}, function(err, status){
+			if(err){
+				res.send(err);
+			} else {
+				console.log("added a bomb!!!");
+
+						// res.json(status);
+			}
+		});
+	// added
+	User.find({name:data.name},function(err, results) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log(results[0].score,"this is result");
+				io.emit("bombAdded",results[0].score)
+			}
+		});
+
+    });
+
+	//next event
+	// socket.on("check", function(data) 
+	// {
+	// 	// console.log("keeping checking");
+	// 	for (var i = 0; i < all.length; i++) {
+	// 		for (var i = 0; i < all[i].length; i++) {
+	// 			console.log(all[i][i]);
+	// 		};
+	// 	};
+
+	// })
+	//next event
+	socket.on("status", function(data) 
+	{
 		console.log(data)
 		io.emit("status", {
 			name: name
 		})
 
-	})
-
-
-
-});
+	})	
+})
--- server/models/bomb.js
+++ server/models/bomb.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
 	name:String,
 	created:{type: Date, default: Date.now },
-	score:0,
+	score:{type: Number, default: 0 },
 	bombs:[{
 		lat:Number,
 		lon:Number
